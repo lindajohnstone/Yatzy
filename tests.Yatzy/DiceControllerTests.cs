caea7651c1ed4d15f748d17dd35383190e9646e4
Sarah Smalley <68258814+sarahsmalley-myob@users.noreply.github.com>
2020-10-23 16:09:23 +1100

    DiceController added (#7)
    * Refactored DiceRollerTest to use InRange
    
    * Test for Reroll method
    
    * not sure what's going on with intersect here?
    
    * Fixed stuff, finished DiceController
    
    Co-authored-by: Linda Johnstone <Linda.Johnstone@myob.com>

--- tests.Yatzy/DiceControllerTests.cs
+++ tests.Yatzy/DiceControllerTests.cs
@@ -1,3 +1,4 @@
+using System.Data;
 using System.Linq;
 using Xunit;
 using Yatzy;
@@ -12,11 +13,34 @@ namespace tests.Yatzy
             // arrange
             var controller = new DiceController();
             // act
-            var result = controller.Roll();
-            var query = result.Where(_ => _ > 0 && _ < 7);
-            var isValidRoll = query.ToArray().Length == 5;
+            controller.RollAllDice();
+            var result = controller.Dice;
             // assert
-            Assert.True(isValidRoll);
+            foreach(var die in result)
+            {
+                Assert.InRange(die, 1, 6);
+            }
+            Assert.Equal(5, result.Length);
+        }
+
+        [Fact]
+        public void Should_Reroll_One_Die()
+        {
+            // arrange
+            var controller = new DiceControllerStub();
+            controller.RollAllDice();
+            var initialRoll = controller.getDiceArray();
+
+            // act
+            controller.RollOneDie(0);
+            var currentRoll = controller.getDiceArray();
+            var intersect = initialRoll.Intersect(currentRoll).ToArray();
+
+            // assert
+            var expectedIntersect = new [] {2, 3, 4, 5};
+
+            Assert.InRange(initialRoll[0], 1, 6);
+            Assert.Equal(expectedIntersect, intersect);
         }
     }
 }
